18b1eb275f295db128db004921fc55b2a9b53a50
hongli750210 <v-hongli1@microsoft.com>
2021-06-28 11:00:31 +0800

    Fix task#409417 [Sample] Azure-Samples/key-vault-java-network-acl by lihong 202106281100

--- src/main/java/com.microsoft.azure/NetworkAclSample.java
+++ src/main/java/com.microsoft.azure/NetworkAclSample.java
@@ -1,20 +1,19 @@
 package com.microsoft.azure;
 
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.ClientSecretCredential;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.keyvault.models.IpRule;
+import com.azure.resourcemanager.keyvault.models.NetworkRuleAction;
+import com.azure.resourcemanager.keyvault.models.NetworkRuleBypassOptions;
+import com.azure.resourcemanager.keyvault.models.NetworkRuleSet;
+import com.azure.resourcemanager.keyvault.models.Vault;
+import com.azure.resourcemanager.keyvault.models.VirtualNetworkRule;
 import com.microsoft.aad.adal4j.AuthenticationException;
-import com.microsoft.azure.AzureEnvironment;
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.keyvault.*;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-
-import com.microsoft.azure.credentials.ApplicationTokenCredentials;
-import com.microsoft.azure.credentials.UserTokenCredentials;
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
-import com.microsoft.rest.LogLevel;
-import com.microsoft.rest.credentials.ServiceClientCredentials;
-import com.microsoft.azure.keyvault.KeyVaultClient;
-import com.microsoft.azure.keyvault.authentication.KeyVaultCredentials;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,13 +21,14 @@ import java.util.List;
 
 public class NetworkAclSample {
 
-    protected static Azure azure;
+    protected static AzureResourceManager azure;
 
     protected static final Region VAULT_REGION = Region.US_WEST;
     protected static final String AZURE_CLIENT_ID = System.getProperty("AZURE_CLIENT_ID");
     protected static final String AZURE_OBJECT_ID = System.getProperty("AZURE_OBJECT_ID");
     protected static final String AZURE_CLIENT_SECRET = System.getProperty("AZURE_CLIENT_SECRET");
     protected static final String AZURE_TENANT_ID = System.getProperty("AZURE_TENANT_ID");
+    protected static final String AZURE_SUBSCRIPTION_ID = System.getProperty("AZURE_SUBSCRIPTION_ID");
 
 
     static {
@@ -37,8 +37,8 @@ public class NetworkAclSample {
     }
 
     public void createSampleVaultWithNetworkAcl() {
-        String rgName = SdkContext.randomResourceName("rg-sample", 24);
-        String vaultName = SdkContext.randomResourceName("vault-sample", 24);
+        String rgName = azure.resourceGroups().manager().internalContext().randomResourceName("rg-sample", 24);
+        String vaultName = azure.resourceGroups().manager().internalContext().randomResourceName("vault-sample", 24);
 
         //Create sample resource group.
         azure.resourceGroups().define(rgName)
@@ -56,7 +56,7 @@ public class NetworkAclSample {
                 .withBypass(NetworkRuleBypassOptions.AZURE_SERVICES)// Allows bypass of network ACLs from Azure services. Valid: 'AzureServices' or 'None'
                 .withDefaultAction(NetworkRuleAction.DENY) // Action to take if access attempt does not match any rule. 'Allow' or 'Deny'
                 //IP rules
-                .withIpRules(new ArrayList<>(Arrays.asList(new IPRule().withValue("0.0.0.0/0")))) // Allow access from all IPv4 addresses
+                .withIpRules(new ArrayList<IpRule>(Arrays.asList(new IpRule().withValue("0.0.0.0/0")))) // Allow access from all IPv4 addresses
                 //Virtual network rules (Allows access to Azure Virtual Networks by their Azure Resource ID).
                 .withVirtualNetworkRules(rules);
 
@@ -65,27 +65,28 @@ public class NetworkAclSample {
                 .withRegion(Region.US_WEST)
                 .withExistingResourceGroup(rgName)
                 .defineAccessPolicy()
-                    .forObjectId(AZURE_OBJECT_ID)
-                    .allowSecretAllPermissions()
-                    .attach()
+                .forObjectId(AZURE_OBJECT_ID)
+                .allowSecretAllPermissions()
+                .attach()
                 .withDeploymentDisabled()
                 .withBypass(NetworkRuleBypassOptions.AZURE_SERVICES)
                 .withDefaultAction(NetworkRuleAction.DENY)
                 .withAccessFromIpAddress("0.0.0.0/0")
                 .create();
 
-        Vault vault2 = azure.vaults().define(SdkContext.randomResourceName("vault-sample", 20))
-                        .withRegion(Region.US_WEST)
-                        .withExistingResourceGroup(rgName)
-                        .defineAccessPolicy()
-                            .forObjectId(AZURE_OBJECT_ID)
-                            .allowSecretAllPermissions()
-                            .attach()
-                        .withDeploymentDisabled()
-                        .withAccessFromAzureServices() //essentially sets Bypass to AZURE_SERVICES
-                        .withAccessFromSelectedNetworks() //Sets default Action to Deny
-                        .withAccessFromIpAddress("0.0.0.0/0")
-                        .create();
+        Vault vault2 = azure.vaults()
+                .define(azure.resourceGroups().manager().internalContext().randomResourceName("vault-sample", 20))
+                .withRegion(Region.US_WEST)
+                .withExistingResourceGroup(rgName)
+                .defineAccessPolicy()
+                .forObjectId(AZURE_OBJECT_ID)
+                .allowSecretAllPermissions()
+                .attach()
+                .withDeploymentDisabled()
+                .withAccessFromAzureServices() //essentially sets Bypass to AZURE_SERVICES
+                .withAccessFromSelectedNetworks() //Sets default Action to Deny
+                .withAccessFromIpAddress("0.0.0.0/0")
+                .create();
 
 
         System.out.println("Created vault " + vault.vaultUri());
@@ -95,17 +96,21 @@ public class NetworkAclSample {
     }
 
     private static void authenticateToAzure() {
+        AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
         // Authentication for general Azure service
-        ApplicationTokenCredentials credentials = new ApplicationTokenCredentials(AZURE_CLIENT_ID,
-                AZURE_TENANT_ID, AZURE_CLIENT_SECRET, AzureEnvironment.AZURE);
-
+        ClientSecretCredential credentials = new ClientSecretCredentialBuilder()
+                .clientId(AZURE_CLIENT_ID).tenantId(AZURE_TENANT_ID)
+                .clientSecret(AZURE_CLIENT_SECRET)
+                .build();
         try {
-            azure = Azure.configure().withLogLevel(LogLevel.BASIC).authenticate(credentials).withDefaultSubscription();
+            azure = AzureResourceManager.configure()
+                    .withLogLevel(HttpLogDetailLevel.BASIC)
+                    .authenticate(credentials, profile)
+                    .withSubscription(AZURE_SUBSCRIPTION_ID);
         } catch (Exception e) {
             e.printStackTrace();
             throw new AuthenticationException(
                     "Error authenticating to Azure - check your credentials in your environment.");
         }
     }
-
 }
